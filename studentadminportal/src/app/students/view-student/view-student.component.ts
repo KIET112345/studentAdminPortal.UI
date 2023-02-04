b11a69d29f512eef1323f0f20dd6e013080a3cb6
Kiet112345 <vankietclbk@gmail.com>
2023-02-04 10:30:53 +0700

    end section6

--- studentadminportal/src/app/students/view-student/view-student.component.ts
+++ studentadminportal/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -9,7 +9,7 @@ import { StudentService } from '../student.service';
 @Component({
   selector: 'app-view-student',
   templateUrl: './view-student.component.html',
-  styleUrls: ['./view-student.component.css']
+  styleUrls: ['./view-student.component.css'],
 })
 export class ViewStudentComponent implements OnInit {
   studentId: string | null | undefined;
@@ -20,10 +20,10 @@ export class ViewStudentComponent implements OnInit {
     dateOfBirth: '',
     email: '',
     mobile: 1,
-    genderId:'',
+    genderId: '',
     gender: {
       id: '',
-      description: ''
+      description: '',
     },
     address: {
       id: '',
@@ -33,52 +33,69 @@ export class ViewStudentComponent implements OnInit {
     profileImageUrl: '',
   };
   genderList: Gender[] = [];
-  constructor(private readonly studentService: StudentService, private readonly route: ActivatedRoute, private readonly genderService: GenderService,
-    private snackbar: MatSnackBar) {
-
-  }
+  constructor(
+    private readonly studentService: StudentService,
+    private readonly route: ActivatedRoute,
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(
       (params) => {
-        this.studentId = params.get("id");
+        this.studentId = params.get('id');
         if (this.studentId) {
           this.studentService.getStudent(this.studentId).subscribe(
             (successRespone) => {
-              console.log('get student successRespone ===>', successRespone)
+              console.log('get student successRespone ===>', successRespone);
               this.student = successRespone;
             },
             (errorRespone) => {
               console.log(errorRespone);
             }
-          )
+          );
         }
       },
       (errorParams) => {
-        console.log(errorParams)
+        console.log(errorParams);
       }
-    )
+    );
 
-    this.genderService.getGenderList().subscribe(
-      (successRespone) => {
-        console.log(successRespone)
-        this.genderList = successRespone;
-      }
-    )
+    this.genderService.getGenderList().subscribe((successRespone) => {
+      console.log(successRespone);
+      this.genderList = successRespone;
+    });
   }
-  upDateStudent(): void {
+  onUpdateStudent(): void {
     // call student service to update the student
-    this.studentService.updateStudent(this.student.id, this.student)
-    .subscribe((successResponse) => {
-      console.log(successResponse);
-      // show a notification
-      this.snackbar.open('Student updated successfully', undefined, {
-        duration: 2000
-      })
-    }, (errorResponse) => {
-      // log it
-    })
+    this.studentService.updateStudent(this.student.id, this.student).subscribe(
+      (successResponse) => {
+        console.log(successResponse);
+        // show a notification
+        this.snackbar.open('Student updated successfully', undefined, {
+          duration: 2000,
+        });
+      },
+      (errorResponse) => {
+        // log it
+      }
+    );
+  }
+
+  onDelete(): void {
+    this.studentService.deleteStudent(this.student.id).subscribe(
+      (successResponse) => {
+        this.snackbar.open('Student deleted successfully', undefined, {
+          duration: 2000
+        });
+        setTimeout(() => {
+          this.router.navigateByUrl('students')
+        }, 1500)
+      },
+      (errorResponse) => {
+        // log it
+      }
+    );
   }
 }
-
-
